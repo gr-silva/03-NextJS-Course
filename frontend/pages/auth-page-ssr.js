40b5a5c5cd7df687c83fa7372cdcdeaa94487923
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-19 21:27:02 -0300

    feat: add auth pages and some checks on form

---
+++ frontend/pages/auth-page-ssr.js
@@ -0,0 +1,10 @@
+const AuthPageSSR = (props) => {
+  return (
+    <div>
+      <h1>Auth Page SSR</h1>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default AuthPageSSR;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,20 +1,49 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 export default function HomeScreen() {
+  const router = useRouter();
+  const [values, setValues] = useState({
+    usuario: "omariosouto",
+    senha: "safepassword",
+  });
+
+  const handleChange = (event) => {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    setValues((currentValues) => {
+      return { ...currentValues, [fieldName]: fieldValue };
+    });
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          // router.push("/auth-page-ssr");
+          router.push("/auth-page-static");
+        }}
+      >
         <input
-          placeholder="Usuário" name="usuario"
-          defaultValue="omariosouto"
+          placeholder="Usuário"
+          name="usuario"
+          value={values.usuario}
+          onChange={handleChange}
         />
         <input
-          placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
+          placeholder="Senha"
+          name="senha"
+          type="password"
+          value={values.senha}
+          onChange={handleChange}
         />
+        {/* <>
+          <pre>{JSON.stringify(values, null, 2)}</pre>
+        </> */}
         <div>
-          <button>
-            Entrar
-          </button>
+          <button>Entrar</button>
         </div>
       </form>
     </div>
