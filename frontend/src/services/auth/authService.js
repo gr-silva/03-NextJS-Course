e06735d779c0f681089792c79bf4ace0202e0501
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-20 13:38:39 -0300

    refactor: use switch fetch() to HttpClient function

--- frontend/src/services/auth/authService.js
+++ frontend/src/services/auth/authService.js
@@ -1,17 +1,13 @@
+import { HttpClient } from "../../infra/HttpClient/HttpClient";
+
 export const authService = {
   async login({ username, password }) {
-    return fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
+    return HttpClient(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
+      body: { username, password },
     }).then(async (serverResponse) => {
       if (!serverResponse.ok) throw new Error("Usuário ou senha inválidos!");
-      const body = await serverResponse.json();
+      const body = await serverResponse.body;
       console.log(body);
     });
   },
