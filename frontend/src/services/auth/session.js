69c507fbdbcfa46607ed8b8c7022207649f78272
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-20 22:05:14 -0300

    feat: add static session functions

--- frontend/src/services/auth/session.js
+++ frontend/src/services/auth/session.js
@@ -1,4 +1,7 @@
 import { authService } from "./authService";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 export const withSession = (_function) => {
   return async (ctx) => {
     try {
@@ -21,5 +24,50 @@ export const withSession = (_function) => {
         },
       };
     }
+  };
+};
+
+export const useSession = () => {
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    authService
+      .getSession()
+      .then((userSession) => {
+        setSession(userSession);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return {
+    data: { session },
+    error,
+    loading,
+  };
+};
+
+export const withSessionHOC = (Component) => {
+  return function Wrapper(props) {
+    const router = useRouter();
+    const session = useSession();
+
+    if (!session.loading && session.error) {
+      console.log("Redirecionar usuário para home");
+      router.push("/?error=401");
+    }
+
+    const modifiedProps = {
+      ...props,
+      session: session.data.session,
+    };
+
+    return <Component {...modifiedProps} />;
   };
 };
