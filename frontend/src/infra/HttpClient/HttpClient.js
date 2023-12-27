fb6eaa4947685cad6726d0a2b1c787a59e01f4b6
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-27 00:31:32 -0300

    feat: ensure user is logged when refresh token is valid

--- frontend/src/infra/HttpClient/HttpClient.js
+++ frontend/src/infra/HttpClient/HttpClient.js
@@ -1,17 +1,47 @@
+import { tokenService } from "../../services/auth/tokenService";
+
 export const HttpClient = async (fetchUrl, fetchOptions) => {
-  return fetch(fetchUrl, {
+  const options = {
     ...fetchOptions,
     headers: {
       "Content-Type": "application/json",
       ...fetchOptions.headers,
     },
     body: fetchOptions.body ? JSON.stringify(fetchOptions.body) : null,
-  }).then(async (serverResponse) => {
-    return {
-      ok: serverResponse.ok,
-      status: serverResponse.status,
-      statusText: serverResponse.statusText,
-      body: await serverResponse.json(),
-    };
-  });
+  };
+
+  return fetch(fetchUrl, options)
+    .then(async (serverResponse) => {
+      return {
+        ok: serverResponse.ok,
+        status: serverResponse.status,
+        statusText: serverResponse.statusText,
+        body: await serverResponse.json(),
+      };
+    })
+    .then(async (response) => {
+      if (!fetchOptions.refresh) return response;
+      if (response.status !== 401) return response;
+
+      console.log("Middleware: Rodar código para atualizar o token");
+      const refreshResponse = await HttpClient(
+        "http://localhost:3000/api/refresh",
+        {
+          method: "GET",
+        }
+      );
+      const newAccessToken = refreshResponse.body.data.access_token;
+      const newRefreshToken = refreshResponse.body.data.refresh_token;
+
+      tokenService.save(newAccessToken);
+
+      const retryResponse = await HttpClient(fetchUrl, {
+        ...options,
+        refresh: false,
+        headers: {
+          Authorization: `Bearer ${newAccessToken}`,
+        },
+      });
+      return retryResponse;
+    });
 };
