8651d02ee6eeb576374e30a94ae9fe8ef9596092
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-28 00:32:50 -0300

    feat: ensure pages content are shown to specific roles

--- frontend/pages/auth-page-ssr.js
+++ frontend/pages/auth-page-ssr.js
@@ -1,13 +1,32 @@
 import { withSession } from "../src/services/auth/session";
 
 const AuthPageSSR = (props) => {
+  const checkRoles = (requestedRole) => {
+    const roles = props.session?.roles;
+    if (roles && roles.includes(requestedRole)) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <div>
-      <h1>Auth Page SSR</h1>
-      <p>
-        <a href="/logout">Logout</a>
-      </p>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+      <h1>Auth Page Server Side Render</h1>
+      {checkRoles("admin") ? (
+        <div>
+          <p>
+            <a href="/logout">Logout</a>
+          </p>
+          <pre>{JSON.stringify(props, null, 2)}</pre>
+        </div>
+      ) : (
+        <div>
+          <p>
+            <a href="/logout">Retornar para Home</a>
+          </p>
+          <p>Você não possui permissões para acessar esta página!</p>
+        </div>
+      )}
     </div>
   );
 };
--- frontend/pages/auth-page-static.js
+++ frontend/pages/auth-page-static.js
@@ -1,13 +1,32 @@
 import { withSessionHOC } from "../src/services/auth/session";
 
 const AuthPageStatic = (props) => {
+  const checkRoles = (requestedRole) => {
+    const roles = props.session?.roles;
+    if (roles && roles.includes(requestedRole)) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <div>
       <h1>Auth Page Static</h1>
-      <p>
-        <a href="/logout">Logout</a>
-      </p>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+      {checkRoles("user") ? (
+        <div>
+          <p>
+            <a href="/logout">Logout</a>
+          </p>
+          <pre>{JSON.stringify(props, null, 2)}</pre>
+        </div>
+      ) : (
+        <div>
+          <p>
+            <a href="/logout">Retornar para Home</a>
+          </p>
+          <p>Você não possui permissões para acessar esta página!</p>
+        </div>
+      )}
     </div>
   );
 };
